7b5cf48dfbfd9715358d03fccc380cf30b73398c
Gordie825 <Gordie825@gmail.com>
2019-09-18 13:02:53 +0200

    added comments in javascript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
-
+// code for filter button clicked
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
         
         if(value == "all")
         {
-            //$('.filter').removeClass('hidden');
             $('.filter').show('1000');
         }
         else
         {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+
             $(".filter").not('.'+value).hide('3000');
             $('.filter').filter('.'+value).show('3000');
             
